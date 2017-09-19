8a306e572d2cd8533eb6edf9a7974e16b0c45fe7
Melanie Corchado <melaniecorchado@Melanies-MacBook-Pro.local>
2017-09-19 16:37:04 -0400

    updated

--- models/story.js
+++ models/story.js
@@ -1,29 +1,19 @@
-// Require mongoose
 var mongoose = require("mongoose");
-// Create Schema class
 var Schema = mongoose.Schema;
 
-// Create article schema
-var ArticleSchema = new Schema({
-  // title is a required string
+var storySchema = new Schema({
   title: {
     type: String,
     required: true
   },
-  // link is a required string
   link: {
     type: String,
     required: true
-  },
-  // This only saves one note's ObjectId, ref refers to the Note model
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
   }
 });
 
 // Create the Article model with the ArticleSchema
-var Article = mongoose.model("Article", ArticleSchema);
+var story = mongoose.model("story", ArticleSchema);
 
 // Export the model
 module.exports = Article;
--- server.js
+++ server.js
@@ -8,36 +8,17 @@ mongoose.Promise = Promise;
 
 var app = express();
 
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/week18day3mongoose");
-var db = mongoose.connection;
-
-db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
-});
-
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
-
 
 app.get("/scrape", function(req, res) {
   request("http://www.echojs.com/", function(error, response, html) {
     var $ = cheerio.load(html);
-    $("article h2").each(function(i, element) {
 
       var result = {};
 
       result.title = $(this).children("a").text();
       result.link = $(this).children("a").attr("href");
 
-      var entry = new Article(result);
+      var entry = new story(result);
 
       entry.save(function(err, doc) {
         if (err) {
@@ -49,31 +30,6 @@ app.get("/scrape", function(req, res) {
       });
 
     });
-  });
-  res.send("Scrape Complete");
-});
-
-app.get("/articles", function(req, res) {
-  Article.find({}, function(error, doc) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(doc);
-    }
-  });
-});
-
-app.get("/articles/:id", function(req, res) {
-  Article.findOne({ "_id": req.params.id })
-  .populate("note")
-  .exec(function(error, doc) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(doc);
-    }
   });
 });
 
